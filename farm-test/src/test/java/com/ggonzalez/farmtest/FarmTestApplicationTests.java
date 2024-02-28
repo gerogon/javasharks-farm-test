007bcfc8e4756b74cfe326e3aff720620c9554a8
Gonzalez <geronimo.gonzalez@accenture.com>
2024-02-27 21:10:42 -0300

    Modifico clase FarmStatusReport - Pasa a ser record (feature Java 17)

--- farm-test/src/test/java/com/ggonzalez/farmtest/FarmTestApplicationTests.java
+++ farm-test/src/test/java/com/ggonzalez/farmtest/FarmTestApplicationTests.java
@@ -1,13 +1,27 @@
 package com.ggonzalez.farmtest;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.ggonzalez.farmtest.entity.Egg;
+import com.ggonzalez.farmtest.repository.ChickenRepository;
+import com.ggonzalez.farmtest.repository.EggRepository;
+import com.ggonzalez.farmtest.repository.FarmRepository;
+import com.ggonzalez.farmtest.service.ChickenService;
+import com.ggonzalez.farmtest.service.EggService;
+import com.ggonzalez.farmtest.service.FarmService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class FarmTestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(){
 	}
 
 }
